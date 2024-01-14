62034d822158382d5840366258f4f23e6c2b95e3
athowes <adamthowes@gmail.com>
2024-01-14 21:17:13 +0000

    Get Python in RStudio working!

---
+++ polars/setup.R
@@ -0,0 +1,7 @@
+#' Note: initially ran into problems here because I was using the x86_64 version of R
+#' I installed the aarch64 version following this thread https://community.rstudio.com/t/error-when-using-conda-environment-with-reticulate-incompatible-architecture-have-arm64-need-x86-64/179190/2
+
+install.packages("reticulate")
+library(reticulate)
+reticulate::py_config()
+sessionInfo()

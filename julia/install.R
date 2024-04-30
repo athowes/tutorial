14609c93e4e3075e90b60b2ccec742305b4e90a2
athowes <adamthowes@gmail.com>
2024-04-30 18:16:04 +0100

    Trying and failing to use Julia in RStudio

---
+++ julia/install.R
@@ -0,0 +1,5 @@
+remotes::install_github("Non-Contradiction/JuliaCall#227")
+
+library(JuliaCall)
+
+julia_setup(JULIA_HOME = "/Users/adamhowes/Library/Application Support/org.R-project.R/R/JuliaCall/julia/1.10.2/julia-1.10.2/bin/")
